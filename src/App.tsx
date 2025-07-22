c5c51070a83769bfd5a80a1664f8be4d4dc8d07f
mtm1018 <mtm1018@naver.com>
2025-07-22 12:21:27 +0900

    chors/누락폴더추가

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />1234
-        </a>
+          <img src={viteLogo} className="logo" alt="Vite logo" /></a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
