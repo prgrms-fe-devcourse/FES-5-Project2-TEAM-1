010a55300a84322aa4d21a4bdf131120b8033737
mtm1018 <mtm1018@naver.com>
2025-07-22 17:50:10 +0900

    style/번들 css삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@
 import RightSidebar from './components/Layout/RightSidebar'
 import './App.css'
 import './style/reset.css'
-
 import LeftSidebar from './components/Layout/LeftSidebar';
-import StudyChannel from './pages/Study/StudyChannel';
+
 
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
     <div className='container'>
       <LeftSidebar />
       {/* 컴포넌트 들어오면 됩니다 */}
-      <StudyChannel/>
       <RightSidebar />
     </div>
   );
