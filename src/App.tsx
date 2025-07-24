10b66a84115d8f3a3c9f38e86f29c6146b4e31ee
EunbinJung <ainatube39@gmail.com>
2025-07-24 15:53:05 +0900

    feature/ 배경 이미지 수정 기능 + 기본 style 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import RightSidebar from './components/Layout/RightSidebar'
 import './App.css'
 import './style/reset.css'
 import LeftSidebar from './components/Layout/LeftSidebar';
-import StudyJoinInfomation from './pages/Study/StudyJoinInfomation';
+import Mypage from './pages/Mypage/Mypage';
 
 
 
@@ -16,9 +16,7 @@ function App() {
         <LeftSidebar />
       </nav>
       {/* 컴포넌트 들어오면 됩니다 */}
-      <main className="components">
-        <StudyJoinInfomation />
-      </main>
+      <Mypage />
       <nav className='rightcontainer'>
         <RightSidebar />
       </nav>
