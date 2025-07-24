369b3a5e1f8da05471eaba47d463e170ecbed434
chechoii <choii2e@gmial.com>
2025-07-24 17:48:34 +0900

    chore/supabase 수정

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zugionbtbljfyuybihxk.supabase.co' // 프로젝트 URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1Z2lvbmJ0YmxqZnl1eWJpaHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNDIyNTksImV4cCI6MjA2ODcxODI1OX0.iN1y_uBhWxdOQkOgcxYfROe3ERVKrOyZYTxEf8UOaJA' // 프로젝트 public API 키
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
