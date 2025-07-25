475c359f808e7874cd674de3ade22b0f93a05999
YooJeong01 <ujjh77@naver.com>
2025-07-25 10:01:10 +0900

    feat. 사이드바 제외 컴포넌트 주석 처리

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './style/reset.css'
 import LeftSidebar from './components/Layout/LeftSidebar';
 import StudyMemberChannel from './pages/Study/StudyMemberChannel';
 import StudyChannel from './pages/Study/StudyChannel';
-// import Mypage from './pages/Mypage/Mypage.tsx';
+import Mypage from './pages/Mypage/Mypage.tsx';
 import MainContent from './pages/Mainpage/MainContent';
 import Footer from './pages/Mainpage/Footer';
 
@@ -17,10 +17,10 @@ function App() {
       </nav>
       {/* 컴포넌트 들어오면 됩니다 */}
       {/* <Mypage /> */}
-            <div className="mainWrapper">
+      {/* <div className="mainWrapper">
         <MainContent />
         <Footer />
-      </div>
+      </div> */}
       <nav className='rightcontainer'>
         <RightSidebar />
       </nav>
