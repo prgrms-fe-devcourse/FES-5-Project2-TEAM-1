6fd956fd9918b23282f8e9e3abd3cd627302ad6d
EunbinJung <ainatube39@gmail.com>
2025-07-25 17:41:38 +0900

    feature/ 마이페이지 이름/분야 설정 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import StudyChannel from './pages/Study/StudyChannel';
 import StudyJoinInfomation from './pages/Study/StudyJoinInfomation';
 import StudyMemberChannel from './pages/Study/StudyMemberChannel';
 import Thread from './pages/Study/components/Thread';
+import Mypage from './pages/Mypage/Mypage';
 
 
 function App() {
@@ -18,15 +19,7 @@ function App() {
       </nav>
       {/* 컴포넌트 들어오면 됩니다 */}
 
-      <main className="components">
-        <Routes>
-          <Route path="/study" element={<StudyChannel />} />
-          <Route path="/study/:id" element={<StudyJoinInfomation />} />
-          <Route path='/channel/:id' element={<StudyMemberChannel />} />
-          <Route path='/channel/memeber/:id' element={<StudyMemberChannel />} />
-          <Route path='/channel/thread/:id' element={<Thread/>}/>
-        </Routes>
-      </main>
+      <Mypage />
       <nav className="rightcontainer">
 
         <RightSidebar />
--- src/pages/Mypage/Mypage.tsx
+++ src/pages/Mypage/Mypage.tsx
@@ -7,35 +7,26 @@ import MypageDetails from './MypageDetails';
 import { useEffect, useState } from 'react';
 import supabase from '../../supabase/supabase';
 import type { Tables } from 'src/supabase/database.types';
-
 import MypageChannel from './components/MypageChannel';
 import MypagePeerReview from './components/MypagePeerReview';
 import MypagePost from './components/MypagePost';
 import MypageScrap from './components/MypageScrap';
-
-
-
 // type UserBase = Tables<'user_base'>;
 // type UserProfile = Tables<'user_profile'>;
 // type UserInterest = Tables<'user_interest'>;
 // type UserSocial = Tables<'user_social'>;
-
 type UserProfileWithJoins = Tables<'user_profile'> & {
   interest: Tables<'user_interest'>[];
   social: Tables<'user_social'>[];
 };
-
 export type User = Tables<'user_base'> & {
   profile: UserProfileWithJoins[];
 };
-
 function Mypage() {
-
   const [userData, setUserData] = useState<User | null>(null);
   const [editMode, setEditMode] = useState(false);
-
+  const [showEdit, setShowEdit] = useState(false);
   useEffect(() => {
-
     const fetchUser = async () => {
       const { data, error } = await supabase
         .from('user_base')
@@ -44,16 +35,14 @@ function Mypage() {
           profile: user_profile(
           *,
           interest: user_interest(*),
-          social: user_social(*) 
+          social: user_social(*)
           )
           `)
         .eq('id', 'e6fd8f91-4bc2-40a4-99e2-a821401129e2')
         .single();
-
         if( data ) {
           setUserData( data as unknown as User );
         }
-
         if( error ) {
           console.error( error );
           return null;
@@ -61,19 +50,15 @@ function Mypage() {
     }
     fetchUser();
   }, [ ])
-
   const handleEditUserPage = () => {
     setEditMode( prev => !prev );
   }
-
   const handleSave = async () => {
     setEditMode( false );
   }
-
 //   const handleDeleteUser = () => {
 //     //
 //   }
-
   return (
     <div className={S.container}>
         <div className={S.wrapper}>
@@ -81,25 +66,30 @@ function Mypage() {
             <button type='button' className={S.editBtn} onClick={handleEditUserPage}>
               {editMode ? '취소' : <img src='/icons/edit.svg' alt='수정 버튼' />}
             </button>
-            <MypageProfile 
-              user={userData} 
+            <MypageProfile
+              user={userData}
               editMode={editMode}
               setUserData={setUserData}
             />
-            <MypageName user={userData} />
-            <MypageDetails user={userData} />
-            <MypageInterest user={userData} />
-            <MypageSocial user={userData} />
-
-            <MypagePeerReview/>
-            <MypageChannel/>
-            <MypageScrap/>
-            <MypagePost/>
+            <MypageName
+              user={userData}
+              editMode={editMode}
+              setUserData={setUserData}
+              showEdit={showEdit}
+              setShowEdit={setShowEdit}
+            />
+            <MypageDetails
+              user={userData}
+              editMode={editMode}
+              setUserData={setUserData}
+              />
+            <MypageInterest
+              user={userData} />
+            <MypageSocial
+              user={userData} />
             { editMode && <button type="submit" onClick={handleSave}>완료</button>}
-
         </div>
     </div>
   )
 }
-
 export default Mypage
