5de1f7244d58fb6c08393492b78f621b8b6dc6d7
YooJeong01 <ujjh77@naver.com>
2025-07-26 22:45:15 +0900

    feat. post content 50자 이내로만 보이게 하는 기능.
    - 기존에는 원본 배열을 훼손하는 slice로 jsx에서 바로 처리
    - 배열 복제해서 원본을 훼손하지 않도록 truncate해주는 함수를 추가

--- src/pages/Mypage/components/MypagePost.tsx
+++ src/pages/Mypage/components/MypagePost.tsx
@@ -13,34 +13,55 @@ import S from './MypagePost.module.css';
  */
 
 type Board = Tables<'board'>
+type NewBoard = Pick<Board,'board_id'|'title'|'contents'>;
 
 // 데이터 불러오는 조건 설정해주기(user_id 바뀔때마다?)
 
 function MypagePost() {
   // const [currentUserId, setCurrentUserId] = useState<string|null>(null);
   const [boardContents, setBoardContents] = useState<Board[]|null>(null);
+  const [newBoardContents, setNewBoardContents] = useState<NewBoard[]|null>(null);
   
 
   useEffect(()=>{
     const fetchPostsAndBoards = async() => {
       const postData = await compareUserId('11e880fd-65ca-4778-b8e9-1888c1e65233','post');
+      if(!postData) return console.error('포스트 불러오기 실패');
 
-      const boardData = await Promise.all(postData!.map( async(post)=> {  
+      const boardData = await Promise.all(postData.map( async(post)=> {  
         const {data, error} = await supabase
         .from('board')
         .select('*')
         .eq('board_id',post.board_id)
         .single(); 
+      
+        if(error) return console.error('보드 불러오기 실패');
+      
         return data; 
       }));
+      
       if(!boardData) return;
       setBoardContents(boardData);
     }
 
     fetchPostsAndBoards();
-    console.log('스크랩 & 보드 패치 완료')
+    // console.log('스크랩 & 보드 패치 완료')
   },[])
+  // 스크랩과 같은 생각
 
+
+  useEffect(()=>{
+    if(!boardContents) return;
+    const copyBoardContentsList:string[] = boardContents.map(({contents})=>contents);
+
+    setNewBoardContents(
+      boardContents.map(({board_id, title},idx)=>{
+        return {board_id, title, contents : copyBoardContentsList[idx].slice(0,50)}
+        }
+      )
+    )
+
+  },[boardContents])
 
   return (
     <>
@@ -48,10 +69,10 @@ function MypagePost() {
       <div className={S.postContainer}>
         <ul className={S.postList}>
           {
-            boardContents && boardContents.map(({board_id, title, contents})=>(
+            newBoardContents && newBoardContents.map(({board_id, title, contents})=>(
               <li key={board_id} className={S.post}>
                 <p className={S.postTitle}>{title}</p>
-                <p className={S.postContent}>{contents.length>50 ? contents.slice(0,50) : contents}</p>
+                <p className={S.postContent}>{contents}</p>
               </li>
             ))
           }
