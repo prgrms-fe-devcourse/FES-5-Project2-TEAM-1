04e20a9e7dd1d790fdfbb22c74984737310e3b20
EunbinJung <ainatube39@gmail.com>
2025-07-29 14:58:53 +0900

    feature/ 관심사 추가 기능

--- src/pages/Mypage/MypageInterest.tsx
+++ src/pages/Mypage/MypageInterest.tsx
@@ -6,6 +6,8 @@ import plus from '/icons/plus.svg';
 import { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import InterestDropdown from './components/InterestDropdown';
+import supabase from '@/supabase/supabase';
+import { useToast } from '@/utils/useToast';
 
 interface Props {
   user: User | null;
@@ -15,27 +17,96 @@ interface Props {
 
 function MypageInterest({user, editMode, setUserData}: Props) {
 
-  const [interests, setInterests] = useState('');
+  const [interests, setInterests] = useState<string[] | undefined>(user?.profile[0].interest[0].interest.split(','));
   const [isFive, setIsFive] = useState(false);
   const [plusClicked, setPlusClicked] = useState(false);
 
   const minusRefs = useRef<(HTMLButtonElement | null)[]>([]);
-
+  const divRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const divRef = useRef<HTMLDivElement | null>(null);
+ 
   const userInterest = user && user.profile[0].interest[0];
+  // const interestArray = userInterest.interest.split(',')
+  const { success, error } = useToast();
+
+
+
+  useEffect(() => {
+    if( interests && interests.length >= 5 ) {
+      setIsFive(true);
+    } else {
+      setIsFive(false);
+    }
+
+    gsap.from(divRef.current, {
+      x: 10,
+      opacity: 0,
+      duration: 0.3,
+      ease: 'power1.out',
+    })
+
+  }, [interests])
+
+  useEffect(() => {
+    const validElements = minusRefs.current.filter(el => el !== null) as HTMLButtonElement[];
+    if( validElements.length > 0 ) {
+      gsap.from(validElements, {
+        y: -10,
+        opacity: 0,
+      });
+    }
+  }, [editMode])
+
   if( !userInterest ) {
     return <div className={S.mypageInterest}>Loading...</div>;
-  }
-
-  const interestArray = userInterest.interest.split(',');
-
-  if( interestArray.length >= 5 ) {
-    setIsFive(true);
   }
 
   const handlePlusInterest = () => {
     setPlusClicked(true);
   }
+
+  const handleMinus = async ( index: number) => {
+
+      const text = divRefs.current[index]?.textContent;
+      const { profile_id } = userInterest;
+
+      if( !interests) return;
+      const newInterests = interests.filter(el => el !== text );
+      setInterests(newInterests);
   
+      const { error: minusError } = await supabase
+        .from('user_interest')
+        .update({interest: newInterests.join(',')})
+        .eq('profile_id', profile_id)
+  
+     if( minusError ) {
+      error('업로드 실패!');
+      return;
+     }
+
+     setUserData((prev) => {
+            if( !prev ) return prev;
+
+            const prevInterest = prev.profile[0].interest?.[0] || {};
+
+            return {
+                ...prev,
+                profile: [{
+                     ...prev.profile[0],
+                     interest: [{
+                        ...prevInterest,
+                        interest: newInterests.join(',')
+                     }]
+                }]
+            }
+        })
+
+        success('관심사 제거 성공!');
+
+    }
+
+    console.log( interests );
+
 
   return (
     <>
@@ -43,29 +114,36 @@ function MypageInterest({user, editMode, setUserData}: Props) {
           <h2>관심분야</h2>
           { editMode 
             ? <div className={S.InterestBlock}>
-              { interestArray.map((interest, index) => (
+              { interests && interests.map((interest, index) => (
                 <div key={index} className={E.editInterestBlockWrapper}>
-                  <div className={S.InterestBlockEach}>
+                  <div
+                    key={index}
+                    ref={(el) => { divRefs.current[index] = el }}
+                    className={S.InterestBlockEach}
+                  >
                       {interest}
                   </div>
                     <button 
                       ref={(el) => {minusRefs.current[index] = el}}
                       className={E.editInterestMinusBtn}
+                      onClick={() => handleMinus( index)}
                     >
                       <img src={minus} alt='마이너스 버튼 아이콘' />
                     </button>
                   </div>
               ))}
                 { !isFive && 
-                    <div className={S.InterestBlockEach}>
+                    <div ref={divRef} className={S.InterestBlockEach}>
                       { plusClicked 
                         ? 
                         <InterestDropdown
-                            plusClicked={plusClicked}
                             setPlusClicked={setPlusClicked}
                             userInterest={userInterest}
                             setUserData={setUserData}
-                          />
+                            user={user}
+                            interests={interests}
+                            setInterests={setInterests}
+                            />
                         : <>           
                             <button
                               className={E.editInterestPlusBtn}
@@ -80,7 +158,7 @@ function MypageInterest({user, editMode, setUserData}: Props) {
               </div>  
             : 
             <div className={S.InterestBlock}>
-                { interestArray && interestArray.map((i, idx) => (
+                { interests && interests.map((i, idx) => (
                   <div key={idx}>
                     <div className={S.InterestBlockEach}>{i}</div>
                   </div>
--- src/pages/Mypage/components/InterestDropdown.tsx
+++ src/pages/Mypage/components/InterestDropdown.tsx
@@ -1,33 +1,33 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import S from './InterestDropdown.module.css';
 import back from '/icons/back.svg';
 import interestData from '@/components/data/interestData.json';
 import type { User } from '../Mypage';
 import type { Tables } from '@/supabase/database.types';
+import { useToast } from '@/utils/useToast';
+import supabase from '@/supabase/supabase';
 
 
 
 interface Props {
-    plusClicked: boolean,
     setPlusClicked: (value: boolean) => void;
     userInterest: Tables<'user_interest'>;
     setUserData: React.Dispatch<React.SetStateAction<User | null>>;
+    user: User | null,
+    interests: string[] | undefined,
+    setInterests: React.Dispatch<React.SetStateAction<string[] | undefined>>;
 }
 
 
-function InterestDropdown({ plusClicked, setPlusClicked, userInterest, setUserData }: Props) {
+function InterestDropdown({ setPlusClicked, userInterest, setUserData, interests, setInterests }: Props) {
 
-    const [interest, setInterest] = useState<string[]>(userInterest.interest.split(','));
     const [isTyping, setIsTyping] = useState(false);
     const [filteredInterest, setFilteredInterest] = useState<string[]>([]);
 
     const ulRef = useRef<HTMLUListElement | null>(null);
     const inputRef = useRef<HTMLInputElement | null>(null);
 
-    useEffect(() => {
-
-        console.log( interest );
-    }, [interest]);
+    const { success, error } = useToast();
 
     const filterInterest = ( value: string) => {
 
@@ -48,23 +48,71 @@ function InterestDropdown({ plusClicked, setPlusClicked, userInterest, setUserDa
 
     const handleFilteredInterest = ( e: React.MouseEvent<HTMLLIElement> ) => {
         const text = e.currentTarget.textContent;
-        inputRef.current.value = text;
-        setIsTyping(false);
+        if (text && inputRef.current) {
+            inputRef.current.value = text;
+            // handleAdd(text);
+            setIsTyping(false);
+        }
     }
 
-    const handleInterestSave = () => {
+    const handleInterestSave = async () => {
 
+        if (!inputRef.current) return;
+        if( !interests ) return;
         const value = inputRef.current.value.trim();
-        if( !value ) return;
 
-        setInterest((prev)=> {
-            console.log( prev );
-            return prev;
-        });
+        const lowerInterest = interests.map(i => i.toLowerCase());
+        if (lowerInterest.includes(value.toLowerCase())) {
+            error('관심사 중복!');
+            return;
+        }
 
+        if (interests.length >= 5) {
+            error('관심사는 최대 5개까지 추가할 수 있어요!');
+            return;
+        }
+
+        // 여기서 새 배열을 먼저 계산
+        const updatedInterests = [...interests, value];
+
+        // 상태 업데이트
+        setInterests(updatedInterests);
+
+        const { profile_id } = userInterest;
+        const { error: addError } = await supabase
+            .from('user_interest')
+            .update({interest: interests.join(',')})
+            .eq('profile_id', profile_id);
+        
+        if( addError ) {
+            error('업로드 실패');
+            console.error(addError);
+            return;
+        }
+
+        setUserData((prev) => {
+            if( !prev ) return prev;
+
+            const prevInterest = prev.profile[0].interest?.[0] || {};
+
+            return {
+                ...prev,
+                profile: [{
+                     ...prev.profile[0],
+                     interest: [{
+                        ...prevInterest,
+                        interest: interests.join(',')
+                     }]
+                }]
+            }
+        })
+
+        success('관심사 추가 성공!');
+        if( inputRef.current ) {
+             inputRef.current.value = '';
+        }
         setPlusClicked(false);
     }
-
     
   return (
     <>
