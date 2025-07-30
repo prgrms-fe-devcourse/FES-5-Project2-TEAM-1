d4b6c149c70463a2c977dbe5afc4c15f5c080e7b
YooJeong01 <ujjh77@naver.com>
2025-07-30 19:26:33 +0900

    feat. 프로젝트 디테일 정보란에 렌더링
    - kakao api로 오프라인 장소 선택시 마커 표시 렌더링

---
+++ src/pages/Study/components/KakaoMap.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk"
+
+interface Props {
+  address:string;
+}
+
+function KakaoMap({address}:Props) {
+  const [lat, setLat] = useState<number|null>(null);
+  const [lng, setLng] = useState<number|null>(null);
+  const geocoder = new kakao.maps.services.Geocoder();
+
+  geocoder.addressSearch(address, function(result, status){
+    if(status === kakao.maps.services.Status.OK) {
+
+      setLat(Number(result[0].y));
+      setLng(Number(result[0].x));
+    }
+  })
+
+  useEffect(()=>{
+    console.log('lat: ', lat);
+    console.log(typeof lat);
+    console.log('lng: ', lng);
+  },[lat, lng])
+
+  return (
+    <>
+    {
+     lat && lng && 
+     <Map
+        center={{
+          lat,
+          lng
+        }}
+        style={{width:"240px", height:'140px', borderRadius:'0.7rem'}}
+        level={4}
+      >
+        <MapMarker
+          position={{
+            lat,
+            lng
+          }}
+          image={{
+            src:'https://zugionbtbljfyuybihxk.supabase.co/storage/v1/object/public/assets//marker.png',
+            size:{
+              width: 70,
+              height: 70,
+          }}}
+          
+        />
+      </Map>
+
+    }
+    </>
+  )
+}
+export default KakaoMap
