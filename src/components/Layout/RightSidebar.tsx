f240e12cfe0bfb4dee2feedc6d49ac9e08cd83e1
mtm1018 <mtm1018@naver.com>
2025-07-30 21:21:23 +0900

    style/알림 스타일

--- src/components/Layout/RightSidebar.tsx
+++ src/components/Layout/RightSidebar.tsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '@/auth/AuthProvider';
 import { useEffect, useState } from 'react';
 import supabase from '@/supabase/supabase';
-
+import Notification from './Notification';
 
 type CurrentUser = {
   profileId:string;
@@ -12,13 +12,20 @@ type CurrentUser = {
   id:string;
   profileImage:string;
 }
-function RightSidebar() {
+
+interface Overlay{
+  isOverlay: boolean;
+  setIsOverlay: (value: boolean) => void
+  isNotification: boolean;
+  setIsNotification: (value: boolean) => void;
+}
+
+function RightSidebar({isOverlay,setIsOverlay,isNotification,setIsNotification}:Overlay) {
 
   const { user, isLoading, logout } = useAuth();
   const [currentUser, setCurrentUser] = useState<CurrentUser>({ profileId: '', email: '', id: '', profileImage: '', });
   const navigate = useNavigate()
-  const [isNotification, setIsNotification] = useState(false)
-
+  
   useEffect(() => {
     if (!user) {
       console.log('로그인이 필요합니다')
@@ -60,6 +67,7 @@ function RightSidebar() {
 
   const handleNotification = () => {
     setIsNotification(!isNotification)
+    setIsOverlay(!isOverlay)
   }
 
   return (
@@ -67,12 +75,19 @@ function RightSidebar() {
       <div className={S.height}>
         <div className={S.loginBox}>
           <div className={S.profileImage}>
-            <img src={user ? currentUser.profileImage : '/public/images/여울.png'} alt="프로필" />
+            <img
+              src={user ? currentUser.profileImage : "/public/images/여울.png"}
+              alt="프로필"
+            />
           </div>
           {user ? (
-            <Link to={`/mypage/${currentUser.profileId}`} className={S.loginBoxGreeting} title='마이페이지 이동'>
+            <Link
+              to={`/mypage/${currentUser.profileId}`}
+              className={S.loginBoxGreeting}
+              title="마이페이지 이동"
+            >
               <p>Hello🖐️</p>
-              <h3>{currentUser.email.split('@')[0]}</h3>
+              <h3>{currentUser.email.split("@")[0]}</h3>
             </Link>
           ) : (
             <div className={S.loginBoxGreeting}>
@@ -113,39 +128,21 @@ function RightSidebar() {
         <nav>
           <ul className={S.navListWrap}>
             <li>My menu</li>
-            <li className={S.navList} onClick={handleNotification}>
-              {isNotification &&  (
-                <>
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="2 0 16 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M6.54568 14.606C6.40929 14.8848 6.33496 15.1954 6.33496 15.5167C6.33496 16.2666 6.73995 16.9581 7.39405 17.3249L7.44094 17.3512C8.09879 17.7202 8.90125 17.7202 9.55911 17.3512L9.606 17.3249C10.2601 16.9581 10.6651 16.2666 10.6651 15.5167C10.6651 15.1954 10.5908 14.8848 10.4544 14.606H6.54568Z"
-                      fill="#222222"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M8.36078 2.46313C6.99247 2.46313 5.67803 2.99646 4.69657 3.94989C3.67723 4.94011 3.10205 6.30073 3.10205 7.72186V8.3646C3.10205 10.0259 2.42965 11.6165 1.23802 12.7741C1.03123 12.9749 0.914551 13.251 0.914551 13.5392C0.914551 14.1284 1.39216 14.606 1.98131 14.606L14.8478 14.606C15.4369 14.606 15.9146 14.1284 15.9146 13.5392C15.9146 13.251 15.7979 12.9749 15.5911 12.7741C14.3994 11.6165 13.7271 10.0259 13.7271 8.3646L13.7271 7.72186C13.7271 6.30073 13.1519 4.94011 12.1325 3.94989C11.1511 2.99647 9.83664 2.46313 8.46832 2.46313H8.36078Z"
-                      fill="#222222"
-                    />
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M9.125 2.50428V1.57031C9.125 1.22513 8.84518 0.945313 8.5 0.945313C8.15482 0.945312 7.875 1.22513 7.875 1.57031V2.48566C8.03592 2.47073 8.19808 2.46317 8.36101 2.46317H8.46855C8.68916 2.46317 8.90836 2.47703 9.125 2.50428Z"
-                      fill="#222222"
-                    />
-                  </svg>
-                  <div className={S.notifyPanel}>알림</div>
-                </>
+            <li className={S.navList}>
+              {isNotification && (
+                <div className={S.notifyPanel}>
+                  <h4>Notification</h4>
+                  <div className={S.notificationList}>
+                    <Notification />
+                  </div>
+                </div>
               )}
-              <span className={S.navListText}>
+              <span className={S.navListText} onClick={handleNotification}>
+                <img
+                  src="/icons/notification.svg"
+                  className={S.notifiIcon}
+                  alt="알림아이콘"
+                />
                 <h3>Notification</h3>
               </span>
             </li>
