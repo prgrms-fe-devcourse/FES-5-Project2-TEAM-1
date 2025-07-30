71fbc10530fa81756d179c1573dc0b0389a02b15
YooJeong01 <ujjh77@naver.com>
2025-07-30 19:47:03 +0900

    fix. kakao api script 경고 해결

--- src/pages/Study/components/KakaoMap.tsx
+++ src/pages/Study/components/KakaoMap.tsx
@@ -10,44 +10,31 @@ function KakaoMap({address}:Props) {
   const [lng, setLng] = useState<number|null>(null);
   const geocoder = new kakao.maps.services.Geocoder();
 
-  geocoder.addressSearch(address, function(result, status){
-    if(status === kakao.maps.services.Status.OK) {
-
-      setLat(Number(result[0].y));
-      setLng(Number(result[0].x));
-    }
-  })
-
   useEffect(()=>{
-    console.log('lat: ', lat);
-    console.log(typeof lat);
-    console.log('lng: ', lng);
-  },[lat, lng])
+    if(!window.kakao) return;
+    geocoder.addressSearch(address, function(result, status){
+      if(status === kakao.maps.services.Status.OK) {
+  
+        setLat(Number(result[0].y));
+        setLng(Number(result[0].x));
+      }
+    })
+  }, [address])
 
   return (
     <>
     {
      lat && lng && 
      <Map
-        center={{
-          lat,
-          lng
-        }}
+        center={{ lat, lng }}
         style={{width:"240px", height:'140px', borderRadius:'0.7rem'}}
         level={4}
       >
         <MapMarker
-          position={{
-            lat,
-            lng
-          }}
+          position={{ lat, lng }}
           image={{
             src:'https://zugionbtbljfyuybihxk.supabase.co/storage/v1/object/public/assets//marker.png',
-            size:{
-              width: 70,
-              height: 70,
-          }}}
-          
+            size:{ width: 70, height: 70, }}}
         />
       </Map>
 
