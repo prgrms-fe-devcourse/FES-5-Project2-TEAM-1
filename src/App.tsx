9908595fff74a37e04c64d8a2e2ea1c128be335f
kimym98 <tampler48@gmail.com>
2025-07-31 20:33:36 +0900

    fix/팝업 중첩렌더링 수정

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import StudyMemberChannel from "./pages/Study/StudyMemberChannel";
 import Thread from "./pages/Study/components/Thread";
 import Mypage from "./pages/Mypage/Mypage";
 import StudyJoinInfomation from "./pages/Study/StudyJoinInfomation";
-import ToastProvider from "./components/ToastProvider";
+
 import MainContent from "./pages/Mainpage/MainContent";
 import Footer from "./pages/Mainpage/Footer";
 import Register from "./pages/Register";
@@ -21,67 +21,65 @@ import MangementChannel from "./pages/Study/components/management/ManagementChan
 import BoardWrite from "./pages/BoardForm/BoardWrite";
 import { useState } from "react";
 
-
 function App() {
   const location = useLocation();
   const isAuthPage =
     location.pathname === "/login" || location.pathname === "/register";
-  const [isOverlay, setIsOverlay] = useState(false)
-  const [isNotification, setIsNotification] = useState(false)
-  
-  
+  const [isOverlay, setIsOverlay] = useState(false);
+  const [isNotification, setIsNotification] = useState(false);
+
   return (
-    <ToastProvider>
-      <div className="container">
-        {isOverlay && (
-          <div className="overlay" onClick={() => {
-            setIsNotification(!isNotification)
-            setIsOverlay(!isOverlay)
-          }}></div>
-        )}
-        {!isAuthPage && (
-          <nav className="leftcontainer">
-            {isOverlay && (
-              <div
-                className="overlay"
-                onClick={() => setIsOverlay(false)}
-              ></div>
-            )}
-            <LeftSidebar />
-          </nav>
-        )}
-        <div className="mainWrapper">
-          <Routes>
-            <Route path="/" element={<MainContent />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/study" element={<StudyChannel />} />
-            <Route path="/channel/:id" element={<StudyMemberChannel />}>
-              <Route index element={<StudyJoinInfomation />} />
-              <Route path="memberchannel" element={<StudyMemberChannel />} />
-              <Route path="thread" element={<Thread />} />
-              <Route path="management" element={<Management />}>
-                <Route index element={<MangementChannel />} />
-                <Route path="approve" element={<Approve />} />
-                <Route
-                  path="managementmembers"
-                  element={<ManagementMembers />}
-                />
-              </Route>
+    <div className="container">
+      {isOverlay && (
+        <div
+          className="overlay"
+          onClick={() => {
+            setIsNotification(!isNotification);
+            setIsOverlay(!isOverlay);
+          }}
+        ></div>
+      )}
+      {!isAuthPage && (
+        <nav className="leftcontainer">
+          {isOverlay && (
+            <div className="overlay" onClick={() => setIsOverlay(false)}></div>
+          )}
+          <LeftSidebar />
+        </nav>
+      )}
+      <div className="mainWrapper">
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/study" element={<StudyChannel />} />
+          <Route path="/channel/:id" element={<StudyMemberChannel />}>
+            <Route index element={<StudyJoinInfomation />} />
+            <Route path="memberchannel" element={<StudyMemberChannel />} />
+            <Route path="thread" element={<Thread />} />
+            <Route path="management" element={<Management />}>
+              <Route index element={<MangementChannel />} />
+              <Route path="approve" element={<Approve />} />
+              <Route path="managementmembers" element={<ManagementMembers />} />
             </Route>
-            <Route path="/Write" element={<BoardWrite />} />
-            <Route path="/Write/:id" element={<BoardWrite />} />
-            <Route path="/mypage/:id" element={<Mypage />} />
-          </Routes>
-          {!isAuthPage && <Footer />}
-        </div>
-        {!isAuthPage && (
-          <nav className="rightcontainer">
-            <RightSidebar isOverlay={isOverlay} setIsOverlay={setIsOverlay} isNotification={isNotification} setIsNotification={setIsNotification} />
-          </nav>
-        )}
+          </Route>
+          <Route path="/Write" element={<BoardWrite />} />
+          <Route path="/Write/:id" element={<BoardWrite />} />
+          <Route path="/mypage/:id" element={<Mypage />} />
+        </Routes>
+        {!isAuthPage && <Footer />}
       </div>
-    </ToastProvider>
+      {!isAuthPage && (
+        <nav className="rightcontainer">
+          <RightSidebar
+            isOverlay={isOverlay}
+            setIsOverlay={setIsOverlay}
+            isNotification={isNotification}
+            setIsNotification={setIsNotification}
+          />
+        </nav>
+      )}
+    </div>
   );
 }
 export default App;
