6e1ff17816b5ac1aa8276584f85c77ad6368c48b
YooJeong01 <ujjh77@naver.com>
2025-08-02 21:07:44 +0900

    feat. truncat 함수 지우고 css로만 글 줄임 설정

--- src/pages/Mypage/components/MypagePost.tsx
+++ src/pages/Mypage/components/MypagePost.tsx
@@ -4,36 +4,23 @@ import supabase from "@/supabase/supabase";
 import S from './MypagePost.module.css';
 import { Link } from "react-router-dom";
 
-/**
- * - 마이페이지 포스트 확인
-    - board 테이블의 profile_id와 post테이블의 profile_id와 현재 접속한 유저 id가 같은걸 기준으로, 작성자가 본인인 경우의 글에대해서만 title과 contents 불러오기
-    - title은 모두 보이고
-    - contents는 truncate로 몇글자 이내만 보이게
-    - 리스트로 뿌리기
- */
-
 type Post = Tables<'post'>
 type Board = Tables<'board'>
-type NewBoard = Pick<Board,'board_id'|'title'|'contents'>;
 
 interface Props {
   profileId : string;
 }
 
-// 데이터 불러오는 조건 설정해주기(user_id 바뀔때마다?)
-
 function MypagePost({profileId}:Props) {
-  // const [currentUserId, setCurrentUserId] = useState<string|null>(null);
-  const [boards, setBoards] = useState<Board[]|null>(null);
   const [posts, setPosts] = useState<Post[]|null>(null);
-  const [newBoards, setNewBoards] = useState<NewBoard[]|null>(null);
+  const [boards, setBoards] = useState<Board[]|null>(null);
+
   
   useEffect(()=>{
     if(!profileId) return;
     const fetchPosts = async() => {
       const {data, error} = await supabase.from('board').select('*').eq('profile_id',profileId);
       if(error) return console.error('포스트 불러오기 실패');
-      // console.log(data);
       setPosts(data);
     }
     fetchPosts();
@@ -61,35 +48,19 @@ function MypagePost({profileId}:Props) {
     fetchBoards();
   },[posts])
 
-
-  useEffect(()=>{
-    if(!boards) return;
-    const copyBoardContentsList:string[] = boards.map(({contents})=>contents);
-    const copyBoardtitleList:string[] = boards.map(({title})=>title);
-
-    setNewBoards(
-      boards.map(({board_id},idx)=>{
-        return {board_id, title:copyBoardtitleList[idx].slice(0,20), contents : copyBoardContentsList[idx].slice(0,50)}
-        }
-      )
-    )
-
-  },[boards])
-
-
   return (
     <>
       <h2 className={S.sectionName}>포스트</h2>
       {
-        posts && posts.length > 0 && newBoards ? (
+        posts && posts.length > 0 && boards ? (
           <section className={S.postContainer}>
             <ul className={S.postList}>
               {
-                newBoards && newBoards.map(({board_id, title, contents})=>(
+                boards && boards.map(({board_id, title, contents})=>(
                   <li key={board_id} className={S.post}>
                       <Link to={`/channel/${board_id}`}>
-                      <p className={S.postTitle}>{title}</p>
-                      <p className={S.postContent}>{contents}</p>
+                      <div className={S.postTitle}>{title}</div>
+                      <div className={S.postContent}>{contents}</div>
                     </Link>
                   </li>
                 ))
@@ -98,9 +69,9 @@ function MypagePost({profileId}:Props) {
           </section>
         ) : (
           <div className={S.nothing}>
-            <img src="/images/서치이미지.png" alt="검색 결과 없음" />
+            <img src="/images/emptyContents.png" alt="작성한 포스트 없음" />
             <p>
-              아직 작성한 포스트가 없습니다 🍃🍃🍃<br />
+              아직 작성한 포스트가 없습니다<br />
               스터디에서 모집 글을 작성해보세요!<br />
               
             </p>
