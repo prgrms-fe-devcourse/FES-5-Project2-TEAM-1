2e6606812e34e6d90092341050c98c711eea08fb
mtm1018 <mtm1018@naver.com>
2025-08-03 19:52:40 +0900

    fix/관리채널 레이아웃변경

--- src/pages/Study/components/management/ManagementChannel.tsx
+++ src/pages/Study/components/management/ManagementChannel.tsx
@@ -15,7 +15,6 @@ type Board = Tables<'board'>;
 type PickBoard = Pick<Board,'member'|'board_cls'|'address'|'meeting_time'|'active'>;
 
 function MangementChannel() {
-
   const [category, setCategory] = useState<'0'|'1'|null>("0");
   const [meetingTime, setMeetingTime] = useState<string|null>(null);
   const [members, setMembers] = useState<number>(1);
@@ -48,8 +47,9 @@ function MangementChannel() {
   useEffect(()=>{
     // console.log(projectData);
     if(!projectData) return;
-    const {member, board_cls, address, meeting_time, active} = projectData;
-    setCategory(board_cls);
+    const { member, address, meeting_time, active } = projectData;
+    
+    setCategory('0');
     setMeetingTime(meeting_time);
     setMembers(member);
     setAddress(address);
@@ -61,6 +61,7 @@ function MangementChannel() {
   useEffect(() => {
     console.log("현재 category:", category);
   }, [category]);
+  
   const handleCheckedOnline = () => {
     // address가 null일때
     // 얘를 클릭했을때
@@ -185,184 +186,187 @@ function MangementChannel() {
     <main className={S.mangementChannelContainer}>
       <h1 className={S.contentHeader}>프로젝트 생성</h1>
       <form className={S.projectDetailForm} onSubmit={handleSubmit}>
-        <div className={S.categoryWrap}>
-          <section className={S.category}>
-            <h2 className={S.sectionHeader}>모집 타입</h2>
-            <div className={S.categoryRadio}>
-              <div className={S.study}>
-                <input
-                  type="radio"
-                  name="category"
-                  id="study"
-                  defaultChecked
-                  onChange={() => setCategory("0")}
-                />
-                <label htmlFor="study">스터디</label>
-              </div>
-              <div className={S.project}>
-                <input
-                  type="radio"
-                  name="category"
-                  id="project"
-                  onChange={() => setCategory("1")}
-                />
-                <label htmlFor="project">프로젝트</label>
-              </div>
-            </div>
-          </section>
-          <section className={S.deadline}>
-            <h2 className={S.sectionHeader}>마감 기한</h2>
-
-            {
-              category == null ?
-              (<button type="button" className={S.calendarBtn}>
-                날짜선택
-              </button>
-              ):(
-              category == "0" ? (
-              <button type="button" className={S.calendarBtn}>
-                날짜선택
-              </button>
-            ):(
-            <Calender isHidden={true} callBack={() => {}} />
-             )
-            )
-          }
-            
-       
-          </section>
-        </div>
-        <div className={S.dateContributors}>
-          <section className={S.time}>
-            <h2 className={S.sectionHeader}>모임 시간</h2>
-            <input
-              type="text"
-              name=""
-              id=""
-              value={meetingTime ? meetingTime : ""}
-              placeholder="매주 월요일 저녁 9시"
-              onChange={(e) => {
-                setMeetingTime(e.target.value);
-              }}
-            />
-          </section>
-
-          <section className={S.contributors}>
-            <h2 className={S.sectionHeader}>모집 인원</h2>
-            <div className={S.countButton}>
-              <button
-                className={S.upButton}
-                type="button"
-                onClick={handleDownButton}
-              >
-                -
-              </button>
-              <p>{members}</p>
-              <button
-                className={S.downButton}
-                type="button"
-                onClick={handleUpButton}
-              >
-                +
-              </button>
-            </div>
-          </section>
-        </div>
-
-        <h2 className={S.sectionHeader}>모임 장소</h2>
-        <section className={S.location}>
-          <div className={S.locationRadio}>
-            <div className={S.online}>
+        <section className={S.category}>
+          <h2 className={S.sectionHeader}>모집 타입</h2>
+          <div className={S.categoryRadio}>
+            <div className={S.study}>
               <input
                 type="radio"
-                name="location"
-                id="online"
-                checked={handleCheckedOnline()}
-                onChange={() => {
-                  setIsOffline(false);
-                  setAddress(null);
-                }}
+                name="category"
+                id="study"
+                defaultChecked
+                onChange={() => setCategory("0")}
               />
-              <label htmlFor="online">온라인</label>
+              <label htmlFor="study">스터디</label>
             </div>
-            <div className={S.offline}>
+            <div className={S.project}>
               <input
                 type="radio"
-                name="location"
-                id="offline"
-                checked={handleCheckedOffline()}
-                onChange={() => {
-                  setIsOffline(true);
-                }}
+                name="category"
+                id="project"
+                onChange={() => setCategory("1")}
               />
-              <label htmlFor="offline">오프라인</label>
+              <label htmlFor="project">프로젝트</label>
             </div>
           </div>
-          {isOffline && (
-            <>
+        </section>
+
+        <section className={S.deadline}>
+          <h2 className={S.sectionHeader}>마감 기한</h2>
+
+          <div className={S.calendarBox}>
+            {category == "1" ? (
+              <Calender
+                isHidden={true}
+                callBack={() => {}}
+                shouldFetch={false}
+              />
+            ) : (
               <button
-                className={S.locationButton}
                 type="button"
-                onClick={() => {
-                  setIsOpen((prev) => !prev);
-                }}
+                className={
+                  category === "0" || category === null
+                    ? S.disableBtn
+                    : S.calendarBtn
+                }
               >
-                {address ? (
-                  <p className={S.address}>{address}</p>
-                ) : (
-                  <p>Location</p>
-                )}
+                날짜선택
               </button>
-              {isOpen && (
-                <DaumPostcodeEmbed
-                  onComplete={handleAddAdress}
-                  style={addressStyle}
+            )}
+          </div>
+        </section>
+
+        <section className={S.time}>
+          <h2 className={S.sectionHeader}>모임 시간</h2>
+          <input
+            type="text"
+            name=""
+            id=""
+            value={meetingTime ? meetingTime : ""}
+            placeholder="매주 월요일 저녁 9시"
+            onChange={(e) => {
+              setMeetingTime(e.target.value);
+            }}
+          />
+        </section>
+
+        <section className={S.contributors}>
+          <h2 className={S.sectionHeader}>모집 인원</h2>
+          <div className={S.countButton}>
+            <button
+              className={S.upButton}
+              type="button"
+              onClick={handleDownButton}
+            >
+              -
+            </button>
+            <div>{members}</div>
+            <button
+              className={S.downButton}
+              type="button"
+              onClick={handleUpButton}
+            >
+              +
+            </button>
+          </div>
+        </section>
+
+        <section className={S.location}>
+          <h2 className={S.sectionHeader}>모임 장소</h2>
+          <div className={S.locationRadio}>
+            <div>
+              <div className={S.online}>
+                <input
+                  type="radio"
+                  name="location"
+                  id="online"
+                  checked={handleCheckedOnline()}
+                  onChange={() => {
+                    setIsOffline(false);
+                    setAddress(null);
+                  }}
                 />
-              )}
-            </>
-          )}
+                <label htmlFor="online">온라인</label>
+              </div>
+              <div className={S.offline}>
+                <input
+                  type="radio"
+                  name="location"
+                  id="offline"
+                  checked={handleCheckedOffline()}
+                  onChange={() => {
+                    setIsOffline(true);
+                  }}
+                />
+                <label htmlFor="offline">오프라인</label>
+              </div>
+            </div>
+            {isOffline && (
+              <>
+                <button
+                  className={S.locationButton}
+                  type="button"
+                  onClick={() => {
+                    setIsOpen((prev) => !prev);
+                  }}
+                >
+                  {address ? (
+                    <p className={S.address}>{address}</p>
+                  ) : (
+                    <p>Location</p>
+                  )}
+                </button>
+                {isOpen && (
+                  <DaumPostcodeEmbed
+                    onComplete={handleAddAdress}
+                    style={addressStyle}
+                  />
+                )}
+              </>
+            )}
+          </div>
         </section>
         <button className={S.saveButton} type="submit">
           저장
         </button>
       </form>
-
       <hr className={S.hr} />
-      <div className={S.activeSection}>
-        <h2 className={S.sectionHeader}>모집 활성화</h2>
-        <section className={S.isActive}>
-          <div className={S.active}>
-            <input
-              type="radio"
-              name="isActive"
-              id="active"
-              defaultChecked
-              onChange={handleActive}
-            />
-            <label htmlFor="active">활성화</label>
-          </div>
-          <div className={S.inactive}>
-            <input
-              type="radio"
-              name="isActive"
-              id="inactive"
-              onChange={handleActive}
-            />
-            <label htmlFor="inactive">비활성화</label>
-          </div>
+      <div className={S.controlSection}>
+        <h1>채널관리</h1>
+        <section className={S.activeGroup}>
+          <h2 className={S.sectionHeader}>모집 활성화</h2>
+          <section className={S.isActive}>
+            <div className={S.active}>
+              <input
+                type="radio"
+                name="isActive"
+                id="active"
+                defaultChecked
+                onChange={handleActive}
+              />
+              <label htmlFor="active">활성화</label>
+            </div>
+            <div className={S.inactive}>
+              <input
+                type="radio"
+                name="isActive"
+                id="inactive"
+                onChange={handleActive}
+              />
+              <label htmlFor="inactive">비활성화</label>
+            </div>
+          </section>
         </section>
-      </div>
-
-      <hr className={S.hr} />
-      <div className={S.deleteSection}>
-        <h2 className={S.sectionHeader}>채널 제거</h2>
-        <button
-          className={S.deleteButton}
-          type="button"
-          onClick={handleDeleteChannel}
-        >
-          채널 삭제하기
-        </button>
+        <section className={S.deleteSection}>
+          <h2 className={S.sectionHeader}>채널 제거</h2>
+          <button
+            className={S.deleteButton}
+            type="button"
+            onClick={handleDeleteChannel}
+          >
+            채널 삭제하기
+          </button>
+        </section>
       </div>
     </main>
   );
