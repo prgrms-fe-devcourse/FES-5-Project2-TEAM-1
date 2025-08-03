473679ad6973c1caf98e3d470f70097a4caace4f
mtm1018 <mtm1018@naver.com>
2025-08-03 22:56:48 +0900

    fix/strict모드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import '@/style/main.css'
@@ -9,7 +9,7 @@ import { KakaoLoaderProvider } from './components/context/useKakaoLoaderProvider
 
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ 
     <ToastProvider>
       <BrowserRouter>
         <AuthProvider>
@@ -19,6 +19,6 @@ createRoot(document.getElementById("root")!).render(
         </AuthProvider>
       </BrowserRouter>
     </ToastProvider>
-  </StrictMode>
+
 );
 
