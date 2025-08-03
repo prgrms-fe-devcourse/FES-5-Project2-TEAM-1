4368886cf1e4ac56093fc0927706505e72284117
mtm1018 <mtm1018@naver.com>
2025-08-03 16:27:54 +0900

    fix/추방당한 스터디 보이는것 수정

--- src/pages/team/Team.tsx
+++ src/pages/team/Team.tsx
@@ -20,9 +20,12 @@ function TeamPage() {
   useEffect(() => {
     const fetchTeams = async () => {
       const { data, error } = await supabase
-        .from("board")
+        .from("approve_member")
         .select("*, user_profile(*,user_base(*))")
-        .eq("profile_id", profileId)
+        .match({
+          "profile_id": profileId,
+          'status': 1
+        })
 
       if (error) {
         console.error(error.message);
@@ -32,20 +35,16 @@ function TeamPage() {
     };
 
     if (profileId) fetchTeams();
-   }, [profileId]);
+  }, [profileId]);
 
-
-  if (!myTeams) return 
 
   return (
     <div className={S.container}>
       <h2>My Teams</h2>
       <ul className={S.teamList}>
-        {
+        {myTeams && myTeams.length > 0 ? (
           myTeams.map((team) => {
-            
-            const dDay = DdayCounter(team.deadline ?? '')
-
+            const dDay = DdayCounter(team.deadline ?? "");
             return (
               <li className={S.teamWrap} key={team.board_id}>
                 <div className={S.teaminfoWrap}>
@@ -60,8 +59,8 @@ function TeamPage() {
                       {team.board_cls == null
                         ? "준비중"
                         : team.board_cls == "0"
-                          ? "스터다"
-                          : "프로젝트"}
+                        ? "스터다"
+                        : "프로젝트"}
                     </p>
                     <h3>{team.title}</h3>
                   </div>
@@ -70,18 +69,39 @@ function TeamPage() {
                   {team.deadline == null ? (
                     <p>진행중인 프로젝트가 없습니다</p>
                   ) : new Date(team.deadline).getTime() <= Date.now() ? (
-                    <button type="button" className={S.peerReviewBtn} onClick={() => navigate(`/channel/${team.board_id}`)}>피어리뷰 작성하기</button>
+                    <button
+                      type="button"
+                      className={S.peerReviewBtn}
+                      onClick={() => navigate(`/channel/${team.board_id}`)}
+                    >
+                      피어리뷰 작성하기
+                    </button>
                   ) : (
                     <div>
-                          남은기한 : {dDay[0]}{dDay[1]} 일
+                      남은기한 : {dDay[0]}
+                      {dDay[1]} 일
                     </div>
                   )}
-                  <button type="button" className={S.moveBtn} onClick={() => navigate(`/channel/${team.board_id}`)}>채널이동하기</button>
+                  <button
+                    type="button"
+                    className={S.moveBtn}
+                    onClick={() => navigate(`/channel/${team.board_id}`)}
+                  >
+                    채널이동하기
+                  </button>
                 </div>
               </li>
-            )
-          } 
-          )}
+            );
+          })
+        ) : (
+          <div className={S.MyTeam}>
+            <img src="/images/팬타.webp" alt="" />
+            <div className={S.message}>
+              현재 참여중인 팀이 없습니다
+            
+            </div>
+          </div>
+        )}
       </ul>
     </div>
   );
